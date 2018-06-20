cdbd569d5107ab01aa3685e5d3da501f5b3c7123
vlad moisei <vladmoisei@yahoo.com>
2018-06-20 13:28:40 +0100

    Finish courses section

--- js/design.js
+++ js/design.js
@@ -12,7 +12,7 @@ for (let elem of skillEvaluation) {
  *
  *
  */
-var arrowDownBtn = document.getElementsByClassName("fa-chevron-down")[0];
+var arrowBtn = document.getElementById("arrow__button");
 var arrowUpBtn = document.getElementsByClassName("fa-chevron-up")[0];
 var coursesList = document.getElementsByClassName("courses__list")[0];
 var coursesListElem = document.getElementsByClassName("elem");
@@ -29,18 +29,18 @@ function removeBulletPoints() {
 }
 
 function listVerticalDisplay() {
-	//var coursesListElemShort = document.getElementsByClassName("short__list");
-	// console.log(bulletPointsList.length);
-	//while (coursesListElemShort.length > 0) {
-	//	coursesListElem[0].classList.remove("short__list");
-	//}
 	for (let elem of coursesListElem) {
 		elem.classList.remove("short__list");
+		elem.classList.add("long__list__elem", "bottom__line");
 	}
+	//remove bootom line to the las element
+	coursesListElem[coursesListElem.length-1].classList.remove("bottom__line");
+	//remove 3 points for short version list
+	coursesList.classList.remove("end__points");
 }
 
 function putArrowUp() {
-	var arrowBtn = document.getElementsByClassName("fa-chevron-down")[0];
+	//var arrowBtn = document.getElementsByClassName("fa-chevron-down")[0];
 	arrowBtn.classList.remove("fa-chevron-down");
 	arrowBtn.classList.add("fa-chevron-up");
 }
@@ -60,16 +60,67 @@ function addFadeIn() {
 		elem.classList.add("fadeIn");
 	}
 }
-//Create event listener for extending list of courses
+
+function addCoursesBackGround() {
+	var coursesSection = document.getElementById("courses");
+	coursesSection.classList.add("long__list__courses");
+}
+
 function arrowDownClick() {
 	removeBulletPoints();
 	listVerticalDisplay();
 	putArrowUp();
 	//removeFadeIn();
 	addFadeIn();
+	addCoursesBackGround();
 }
 
-arrowDownBtn.addEventListener('click', arrowDownClick);
+function addBulletPoints() {
+	var bulletPointsList = document.getElementsByClassName("list__point");
+	console.log(bulletPointsList.length);
+	for (let i = 0; i < bulletPointsList.length; i++) {
+		bulletPointsList[i].classList.add("bullet");
+	}
+	//coursesList.classList.remove("long__list");
+}
+
+function listHorizontalDisplay() {
+	for (let elem of coursesListElem) {
+		elem.classList.add("short__list");
+		elem.classList.remove("long__list__elem", "bottom__line");
+	}
+	coursesList.classList.add("end__points");
+	//remove bootom line to the las element
+	//coursesListElem[coursesListElem.length-1].classList.add("bottom__line");
+}
+
+function putArrowDown() {
+	arrowBtn.classList.remove("fa-chevron-up");
+	arrowBtn.classList.add("fa-chevron-down");
+}
+
+function removeCoursesBackGround() {
+	var coursesSection = document.getElementById("courses");
+	coursesSection.classList.remove("long__list__courses");
+}
+
+function arrowUpClick() {
+	addBulletPoints();
+	listHorizontalDisplay();
+	putArrowDown();
+	addFadeIn();
+	removeCoursesBackGround();
+}
+//Create event listener for extending list of courses
+function arrowClick() {
+	if (arrowBtn.classList.contains("fa-chevron-down"))
+		arrowDownClick();
+	else if (arrowBtn.classList.contains("fa-chevron-up"))
+		arrowUpClick();
+	setTimeout(function() { removeFadeIn(); }, 1100);
+}
+
+arrowBtn.addEventListener('click', arrowClick);
 
 (function addAnimatedFadeIn() {
 
